0f26ccb64f4f4a7b49646a494113ba7101ff2fd6
vaquero <froilan@virtual-artifact.com>
2024-10-03 14:28:12 +0100

    fix: add controlnet conditioning scale to endpoint paramters

--- predict.py
+++ predict.py
@@ -39,6 +39,7 @@ class Predictor(BasePredictor):
         height: int = Input(description="The height in pixels of the generated image", default=512),
         width: int = Input(description="The width in pixels of the generated image", default=512),
         controlnet_image: str = Input(description="Controlnet image encoded in b64 string for guiding image generation", default=""),
+        controlnet_conditioning_scale: float = Input(description="The outputs of the ControlNet are multiplied by controlnet_conditioning_scale before they are added to the residual in the original unet", default=1)
     ) -> str:
         """Run a single prediction on the model"""
         # processed_input = preprocess(image)
@@ -62,24 +63,22 @@ class Predictor(BasePredictor):
         else:
             image_data = base64.b64decode(controlnet_image)
             canny_image = Image.open(BytesIO(image_data))
-            return "true"
 
-        return "test"
         # generate images
-        # image = self.pipe(
-        #     prompt, 
-        #     image=canny_image,
-        #     negative_prompt=negative_prompt,
-        #     controlnet_conditioning_scale=controlnet_conditioning_scale, 
-        #     num_inference_steps=steps,
-        #     guidance_scale=guidance_scale,
-        #     width=width,
-        #     height=height,
-        #     image_encoding=image_encoding,
-        # ).images[0]
-        #
-        # buffered = BytesIO()
-        # image.save(buffered, format=image_encoding)
-        # img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
-        #
-        # return img_str
+        image = self.pipe(
+            prompt, 
+            image=canny_image,
+            negative_prompt=negative_prompt,
+            controlnet_conditioning_scale=controlnet_conditioning_scale, 
+            num_inference_steps=steps,
+            guidance_scale=guidance_scale,
+            width=width,
+            height=height,
+            image_encoding=image_encoding,
+        ).images[0]
+
+        buffered = BytesIO()
+        image.save(buffered, format=image_encoding)
+        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+        return img_str
